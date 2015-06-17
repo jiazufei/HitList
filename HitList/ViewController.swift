4f019c4e5c24ad62514304cf9c85e97c92345f0e
小坏蛋L <efei_12341@sina.com>
2015-06-17 13:51:47 +0800

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by CaoFei on 15/6/17.
+//  Copyright (c) 2015年 CaoFei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
